7d945c9b85f4397c994aa427205a9dacfbb347ba
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-11-06 11:01:05 -0300

    Restart from scratch and Add Flexbox

---
+++ app/index.tsx
@@ -0,0 +1,44 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={ styles.appContainer }>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal!" 
+          style={ styles.textInput }/>
+        <Button title="Add Goal" />
+      </View>
+      <View style={ styles.goalsContainer}>
+          <Text>
+            List of goals...
+          </Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    padding: 6
+  },
+  goalsContainer: {
+    flex: 3
+  }
+})
